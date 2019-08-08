0eb60f60712bc8725935dd2270f6f2dce335ca7d
Sravya Balasa <sravya.balasa@gmail.com>
2019-08-08 15:09:16 -0700

    THE NAVBAR STICKS

--- style/js/smooth-scroll.js
+++ style/js/smooth-scroll.js
@@ -1,1 +1,25 @@
+$( document ).ready(function() {
 
+  // Fading in the top on load
+  fadeHome();
+
+});
+
+function fadeHome() {
+  var x = document.getElementsByClassName("heading")[0];
+  var y = document.getElementsByClassName("subheading")[0];
+  var z = document.getElementsByClassName("socials")[0];
+
+  $(x).fadeTo(3000,1);
+  $(y).fadeTo(3000,1);
+  $(z).fadeTo(3000,1,fadeTop);
+}
+
+function fadeTop() {
+  var x = document.getElementsByClassName("nav")[0];
+  var y = document.getElementsByClassName("initials")[0];
+
+  $(x).animate({opacity: 1}, 600);
+  $(y).animate({opacity: 1}, 600);
+
+}
